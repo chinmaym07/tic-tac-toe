937f46f0a0e6134a1318bd04640b0050f0f10061
chinmaym07 <b418020@iiit-bh.ac.in>
2020-10-09 20:07:29 +0530

    Adding All Files[C

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
         <h1>Tic Tac Toe</h1>
       </div>
       <Switch>
-        <Route exact path='/' render={(routeProps) => (<PlayerLogin player1={player1} player2={player2} setPlayer1={setPlayer1} setPlayer2={setPlayer2} {...routeProps}/>)} />
-        <Route path='/game'render={(routeProps)=>(<Board player1={player1} player2={player2} {...routeProps}/>)} />
+        <Route exact path='https://chinmaym07.github.io/tic-tac-toe/' render={(routeProps) => (<PlayerLogin player1={player1} player2={player2} setPlayer1={setPlayer1} setPlayer2={setPlayer2} {...routeProps}/>)} />
+        <Route path='https://chinmaym07.github.io/tic-tac-toe/game'render={(routeProps)=>(<Board player1={player1} player2={player2} {...routeProps}/>)} />
       </Switch>
       <Footer />
     </div>
